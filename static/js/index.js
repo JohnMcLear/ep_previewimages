c113ffdd8fe5dd0c662d92d88f2b3120ed1ded0a
John McLear <john@mclear.co.uk>
2020-11-22 17:21:43 +0000

    autofixes from Etherpad checkPlugins.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,27 +1,25 @@
-exports.aceGetFilterStack = function(name, context){
+exports.aceGetFilterStack = function (name, context) {
   return [
     context.linestylefilter.getRegexpFilter(
-      new RegExp("\\bhttps?://\\S+\\.([pP][nN][gG]|[jJ][pP][eE]?[gG]|[gG][iI][fF]|[bB][mM][pP]|[sS][vV][gG])([?&;]\\S*|(?=\\s|$))", "g"), 'image')
+        new RegExp('\\bhttps?://\\S+\\.([pP][nN][gG]|[jJ][pP][eE]?[gG]|[gG][iI][fF]|[bB][mM][pP]|[sS][vV][gG])([?&;]\\S*|(?=\\s|$))', 'g'), 'image'),
   ];
-}
+};
 
 
-exports.aceCreateDomLine = function(name, args){
+exports.aceCreateDomLine = function (name, args) {
   if (args.cls.indexOf('image') > -1) { // If it's an image
-    var src;
-    cls = args.cls.replace(/(^| )image:(\S+)/g, function(x0, space, image) {
+    let src;
+    cls = args.cls.replace(/(^| )image:(\S+)/g, (x0, space, image) => {
       src = image;
-      return space + "image image_" + image;
+      return `${space}image image_${image}`;
     });
 
 
     // Note the additional span wrapper here is required to stop errors if someone types text after an image url
     return [{
-      cls: cls,
-      extraOpenTags: '<span style="display:block;"><img src="' + src + '" style="max-width:100%" /></span>',
-      extraCloseTags:''
+      cls,
+      extraOpenTags: `<span style="display:block;"><img src="${src}" style="max-width:100%" /></span>`,
+      extraCloseTags: '',
     }];
   }
-}
-
-
+};
